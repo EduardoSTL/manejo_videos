b69ec05982e46ff02adf4c45c1e9e826b5f83893
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-09-22 11:26:13 -0600

    added: discover provider

---
+++ lib/presentation/providers/discover_provider.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:manejo_videos/domain/entitites/video_post.dart';
+import 'package:manejo_videos/infrastructure/models/local_video_model.dart';
+import 'package:manejo_videos/shared/data/local_video_post.dart';
+
+class DiscoverProvider extends ChangeNotifier{
+  bool initialLoading = true;
+  List<VideoPost> videos = [];
+
+  Future<void> loadNextPage() async{
+    //DB repository
+    await Future.delayed(const Duration(seconds: 2));
+     final List<VideoPost> newVideos = videoPosts.map(
+      (video) => LocalVideoModel.fromJsonMap(video).toVideoPostEntity()).toList();
+
+      videos.addAll(newVideos);
+      initialLoading=false;
+      notifyListeners();
+  }
+}
