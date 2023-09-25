d02349d11eb368dc7e1ba6f2d0ab71e0910c3287
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-09-25 09:06:52 -0600

    updt

--- lib/presentation/widget/shared/video_buttons.dart
+++ lib/presentation/widget/shared/video_buttons.dart
@@ -10,7 +10,7 @@ class VideoButtons extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Column(
       children: [
         IconButton(onPressed: (){}, icon: Icon(Icons.favorite, color: Colors.red)),
       ],
