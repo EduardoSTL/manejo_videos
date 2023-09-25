678549c2ab752d5ac3b73b8ad148c7ef06b73aab
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-09-25 12:11:23 -0600

    updagdbbwebf

--- lib/presentation/widget/video/fullscreen_player.dart
+++ lib/presentation/widget/video/fullscreen_player.dart
@@ -30,6 +30,12 @@ class _FullScreenPlayerState extends State<FullScreenPlayer> {
   }
 
   @override
+  void dispose(){
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container();
   }
