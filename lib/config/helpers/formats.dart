452d6e455569f378afbb287ea97e530d5c6be071
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-09-25 10:53:33 -0600

    added formats

---
+++ lib/config/helpers/formats.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Formats {
+  static String formatsReadeableNumber(double number){
+    final formatterNumber = NumberFormat.compactCurrency(
+
+    );
+    return '';
+  } 
+
+}
